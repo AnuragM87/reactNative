4c230b2ffca14547340f91e18d37d3592bd036fc
Anurag Chaudhary <anuragchaudharyM87@gmail.com>
2023-05-16 19:23:38 +0530

    initial change

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import {view ,Text,StyleSheet} from 'react-native'
+import {View ,Text,StyleSheet,FlatList} from 'react-native'
 
+//props are config option we specify for jsx
 
 cosnt ListScreen=()=>{
-    return <Text>haan bhai</Text>
+    const Frnds=[
+        {name: "f1"},
+        {name: "f11"},
+        {name: "f111"},
+        {name: "f1111"},
+        {name: "f112"},
+        {name: "f1323"},
+        {name: "f123"},
+        {name: "f1233"}
+    ]
+    return(
 
-}
+  <FlatList 
+  data={Frnds} 
+  renderItem={({item})=>{
+    return<Text >{item.name}</Text>
+  }}/>
 
+    )
 const style =StyleSheet.create({})
 
 
