a5954de6ee27e4315d82690d3f3603360aeb52d5
Anurag Chaudhary <116382864+AnuragM87@users.noreply.github.com>
2023-05-21 01:54:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ComponentScreen from "./src/screens/ComponentScreen";
+import CounterClock from "./src/screens/CounterClock";
+import ImageScreen from "./src/screens/ImageScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List: ListScreen,
+    Component: ComponentScreen,
+    Counter: CounterClock,
+    Images:ImageScreen,
+    RandomColor:ColorScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
